f972eb03422f6825ea547ec776095a2bed22f38b
adriankoder <adrian.koder@gmail.com>
2024-04-12 12:30:18 +0200

    test

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
+let condition = true;
+
 let smileys = ['😀', '😃', '😄', '😁', '😆'];
 
 function copySmiley(event) {
     let button = event.target;
-    let = emoji = button.textContent.replace('copy', '');
+    let emoji = button.textContent.replace('copy', '');
 
     navigator.clipboard.writeText(emoji).then(function() {
-        console.log('Copying to clipboard was successful!');
-    }
-    , function(err) {
-        console.error('Could not copy text: ', err);
     });
+}
+
+if (condition) {
+    for (let j = 0; j < 2; j++) {
+        for (let i = 0; i < smileys.length; i++) {
+            let p = document.createElement('p');
+            
+            p.textContent = smileys[i];
+            
+            document.querySelector('#smileyContainer').appendChild(p);
+        }
+    }
+} else {
+    console.log("Condition not met");
 }
